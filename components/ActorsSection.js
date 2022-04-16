0cfc0a23df81edc21fc03fff0184609e5fe83c15
Gordon Huang <dqna64@gmail.com>
2022-04-16 18:32:12 +1000

    home page and section styled component

---
+++ components/ActorsSection.js
@@ -0,0 +1,10 @@
+import { SectionContainer, SectionTitle } from "../pages/Section"
+
+export default function ActorsSection() {
+  return (
+    <SectionContainer>
+    <SectionTitle>Actors</SectionTitle>
+    <p>Who are the culprits behind cybersecurity incidents?</p>
+  </SectionContainer>
+  )
+}
---
+++ pages/Section.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const SectionTitle = styled.h2`
+  font-size: 2.5rem;
+  @media (min-width: 1024px) {
+    font-size: 3rem;
+  }
+`
+
+export const SectionContainer = styled.div`
+  margin-top: 1rem;
+`
